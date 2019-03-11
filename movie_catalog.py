c974b5e482ed5985f49630d228d7738be45c60fa
MoShabana <M.Shabana@protonmail.ch>
2019-03-11 23:20:47 +0200

    Created movie genres pages

--- movie_catalog.py
+++ movie_catalog.py
@@ -9,3 +9,15 @@ def home():
     genres = Genre.query.all()
     movies = Movie.query.order_by(Movie.date_posted.desc()).limit(5).all()
     return render_template('home.html', genres=genres, movies=movies)
+
+
+@app.route('/genre/<int:genre_id>')
+def genre(genre_id):
+    genres = Genre.query.all()
+    genre = Genre.query.filter_by(id=genre_id).first()
+    movies = Movie.query.filter_by(genre=genre).all()
+    return render_template('genre.html',
+                           genres=genres,
+                           genre=genre,
+                           movies=movies,
+                           title=f'{genre.name} Movies')
