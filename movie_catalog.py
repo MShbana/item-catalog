51b8a4b25fe19aa0118f95ea76e1ad3a2048034a
MoShabana <M.Shabana@protonmail.ch>
2019-03-11 19:45:49 +0200

    Changed genres from dummydata to data from database

--- movie_catalog.py
+++ movie_catalog.py
@@ -1,13 +1,15 @@
 from flask import render_template
+from models import db, Movie, Genre
 from setup import app
 
 
 # Dummy Data #
-genres = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary'	, 'Drama', 'Family', 'Fantasy', 'Game Show', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'News', 'Reality-TV', 'Romance', 'Sci-Fi', 'Sport'	, 'Superhero', 'Talk Show', 'Thriller', 'War', 'Western']
+# genres = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary'	, 'Drama', 'Family', 'Fantasy', 'Game Show', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'News', 'Reality-TV', 'Romance', 'Sci-Fi', 'Sport'	, 'Superhero', 'Talk Show', 'Thriller', 'War', 'Western']
 movies = ['Home Alone', 'The Purge', 'Rush Hour', "The God Father", "Eternal Sunshine of the Spottless Mind"]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
+    genres = Genre.query.all()
     return render_template('home.html', genres=genres, movies=movies)
