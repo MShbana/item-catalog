c4c949fce556091d70feb34da9c0ee326b71851c
MoShabana <M.Shabana@protonmail.ch>
2019-03-12 19:28:33 +0200

    Removed default values for nullable genre table rows

--- models.py
+++ models.py
@@ -17,10 +17,9 @@ class Movie(db.Model):
     title = db.Column(db.String(50), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False,
                             default=datetime.utcnow)
-    release_year = db.Column(db.Integer, nullable=False,
-                             default='Undetermined')
+    release_year = db.Column(db.Integer, nullable=False)
     director = db.Column(db.String(30), nullable=False, default='Anonymous')
-    duration = db.Column(db.Integer, nullable=False, default='Undetermined')
+    duration = db.Column(db.Integer, nullable=False)
     rate = db.Column(db.Integer, nullable=False)
     storyline = db.Column(db.Text, nullable=False)
     poster = db.Column(db.String(20), nullable=False,
