abd48e6a7775880141843b39e8ff8c7cfc90bd43
MoShabana <M.Shabana@protonmail.ch>
2019-03-18 11:18:57 +0200

    Fixed some code styling issues in forms.py

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 import datetime
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import (StringField, IntegerField, SelectField, 
-                     TextAreaField, SubmitField,validators)
+from wtforms import (StringField, IntegerField, SelectField,
+                     TextAreaField, SubmitField, validators)
 from wtforms.validators import (InputRequired, Regexp, Length,
                                 NumberRange, Optional)
 
@@ -10,99 +10,78 @@ from wtforms.validators import (InputRequired, Regexp, Length,
 class BaseMovieForm(FlaskForm):
     current_year = datetime.datetime.now().year
 
+    dir_reg = ('^([a-zA-Z][a-zA-Z\s.-]*[a-zA-Z])$')
+    dir_err = ('Director name may only contain [letters, spaces, hyphens,'
+               ' periods] and should begin and end with a letter.')
+    h_err = 'Duartion Hours must be between 0 and 10.'
+    m_err = 'Duration Minutes must be between 0 and 60.'
+
     director = StringField('Director',
-                            render_kw={
-                                'placeholder': 'Movie Director'
-                            },
-                            validators=[
+                           render_kw={'placeholder': 'Movie Director'},
+                           validators=[
                                 InputRequired(),
-                                Regexp('^([a-zA-Z][a-zA-Z\s.-]*[a-zA-Z])$', message='Director name may only contain [letters, spaces, hyphens, periods] and should begin and end with a letter.'),
-                                Length(min=2, max=30)
-                            ]
-    )
+                                Regexp(dir_reg, message=dir_err),
+                                Length(min=2, max=30)])
     release_year = SelectField('Release Year',
                                coerce=int,
-                               choices=[(year, year) for year in range(1877, current_year)],
-                               validators=[
-                                    InputRequired()
-                               ]
-    )
+                               choices=[(year, year)
+                                        for year in range(1877, current_year)],
+                               validators=[InputRequired()])
     genre = SelectField('Genre',
                         choices=[],
-                        validators=[
-                            InputRequired()
-                        ]
-    )
+                        validators=[InputRequired()])
     duration_hrs = IntegerField('hrs',
-                                render_kw={
-                                    'placeholder': 'Duration in Hours'
-                                },
+                                render_kw={'placeholder': 'Duration in Hours'},
                                 validators=[
                                     InputRequired(),
-                                    NumberRange(min=0, max=10, message='Duartion Hours must be between 0 and 10.')
-                                ]
-    )
+                                    NumberRange(min=0, max=10, message=h_err)])
     duration_mins = IntegerField('mins',
                                  render_kw={
-                                     'placeholder': 'Duration in Minutes'
-                                 },
+                                    'placeholder': 'Duration in Minutes'},
                                  validators=[
-                                     InputRequired(),
-                                     NumberRange(min=0, max=60, message='Duration Minutes must be between 0 and 60.')
-                                 ]
-    )
+                                    InputRequired(),
+                                    NumberRange(min=0, max=60, message=m_err)])
     rate = SelectField('Rate',
                        coerce=int,
                        choices=[(rate, rate) for rate in range(1, 11)],
                        default=10,
-                       validators=[
-                           InputRequired()
-                       ]
-    )
+                       validators=[InputRequired()])
     poster = FileField('Poster',
-                       validators=[
-                           Optional(),
-                           FileAllowed(['jpg', 'jpeg', 'png'])
-                       ]
-    )
+                       validators=[Optional(),
+                                   FileAllowed(['jpg', 'jpeg', 'png'])])
     storyline = TextAreaField('Storyline',
-                              validators=[
-                                  InputRequired(),
-                                  Length(min=50, max=1000)
-                              ]
-    )
+                              validators=[InputRequired(),
+                                          Length(min=50, max=1000)])
 
 
 class NewMovieForm(BaseMovieForm):
+    title_reg = '^([a-zA-Z0-9][a-zA-Z0-9\s:.-]*[a-zA-Z0-9])$'
+    title_err = ('Title may only contain [letters, numbers, hyphens, periods,'
+                 ' colons, spaces] and should begin and end'
+                 ' with a letter or number.')
+
     title = StringField('Title',
-                        render_kw={
-                            'placeholder': 'Movie Title'
-                        },
+                        render_kw={'placeholder': 'Movie Title'},
                         validators=[
                             InputRequired(),
-                            Regexp('^([a-zA-Z0-9][a-zA-Z0-9\s:.-]*[a-zA-Z0-9])$', message='Title may only contain [letters, numbers, hyphens, periods, colons, spaces] and should begin and end with a letter or number.'),
-                            Length(min=2, max=50)
-                        ]
-    )
+                            Regexp(title_reg, message=title_err),
+                            Length(min=2, max=50)])
     submit = SubmitField('Post')
 
+
 class UpdateMovieForm(BaseMovieForm):
+    title_reg = '^([a-zA-Z0-9][a-zA-Z0-9\s:.-]*[a-zA-Z0-9])$'
+    title_err = ('Title may only contain [letters, numbers, hyphens, periods,'
+                 ' colons, spaces] and should begin and end'
+                 ' with a letter or number.')
+
     title = StringField('Title',
-                        render_kw={
-                            'readonly': True
-                        },
+                        render_kw={'readonly': True},
                         validators=[
                             InputRequired(),
-                            Regexp('^([a-zA-Z0-9][a-zA-Z0-9\s:.-]*[a-zA-Z0-9])$', message='Title may only contain [letters, numbers, hyphens, periods, colons, spaces] and should begin and end with a letter or number.'),
-                            Length(min=2, max=50)
-                        ]
-    )
+                            Regexp(title_reg, message=title_err),
+                            Length(min=2, max=50)])
     genre = SelectField('Genre',
-                        render_kw={
-                            'readonly':True
-                        },
-                        validators=[
-                            InputRequired()
-                        ]
-    )
+                        render_kw={'readonly': True},
+                        validators=[InputRequired()])
     submit = SubmitField('Update')
