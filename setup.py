4d47c19577177806f7a4557a083f0ac847546ebc
mshbana <mshbana@pm.me>
2020-03-02 17:04:33 +0200

    Enable debug

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.debug = True
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_message = 'You do not have permission to access that page. Please Log in to continue.'
