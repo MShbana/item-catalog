7d4ff87a3430200f9823f90f44aea9ee1d68ce0b
mshbana <mshbana@pm.me>
2020-03-03 17:30:58 +0200

    Fix bug

--- models.py
+++ models.py
@@ -32,7 +32,7 @@ class User(db.Model, UserMixin):
 class Genre(db.Model):
     __tablename__ = 'genre'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True, nullable=False)
+    name = db.Column(db.String(1000), unique=True, nullable=False)
     movies = db.relationship('Movie')
 
     def __repr__(self):
