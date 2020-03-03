5f8224e90da2b4ee4767c4a9dc240f3a1c03c61e
mshbana <mshbana@pm.me>
2020-03-03 16:34:26 +0200

    Fix database url config var

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['DEBUG'] = True
 app.debug = True
 db = SQLAlchemy(app)
